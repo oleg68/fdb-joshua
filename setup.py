7bd0ebb0e65e1f6a77a8694ed313e8840b06279e
Aaron Molitor <amolitor@apple.com>
2021-04-06 17:20:29 -0500

    discretely set fdb version as coordinating clusters are not at 6.3 yet

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ all_modules = [
     Module(
         'joshua',
         'Joshua - a supercomputer that runs simulations of war^H^H^Hdatabases',
-        ['argparse', 'foundationdb>=6.1.11', 'subprocess32'], [],
+        ['argparse', 'foundationdb==6.2.10', 'subprocess32'], [],
         [childsubreaper], ['Operating System :: POSIX :: Linux'])
 ]
 
